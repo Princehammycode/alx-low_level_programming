f1bd2f5cfba452e625a75bddf1ffce4dda33a123
Princehammycode <princehammond2579@gmail.com>
2023-02-17 19:27:34 -0800

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,13 +16,13 @@ if (n > 0)
 {
 printf("%d is positive\n", n);
 }
-if (n < 0)
-{
-printf("%d is negative\n", n);
-}
-if (n == 0)
+else if (n == 0)
 {
 printf("%d is zero\n", n);
+}
+else
+{
+printf("%d is negative\n", n);
 }
 return (0);
 }
